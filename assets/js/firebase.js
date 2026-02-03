e448fcc178bdf31704a36bc12607bdda0dc6e0bb
jatinkumar13 <jatinkumar66427@gmail.com>
2026-02-03 09:51:39 +0530

    Add files via upload

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCcEzGdDs2HlG2G-mK1cSRZRldvsTF1XEI",
+  authDomain: "jaidevedit.firebaseapp.com",
+  projectId: "jaidevedit",
+  storageBucket: "jaidevedit.appspot.com",
+  messagingSenderId: "728216514466",
+  appId: "1:728216514466:web:11604a54f726be80450cfb"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
