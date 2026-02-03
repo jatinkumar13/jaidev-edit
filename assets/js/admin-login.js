b32667ac7126cd057094b4a5665163f97bcc7e4f
jatinkumar13 <jatinkumar66427@gmail.com>
2026-02-03 10:08:42 +0530

    Update admin-login.js

--- assets/js/admin-login.js
+++ assets/js/admin-login.js
@@ -1,6 +1,6 @@
 import { auth } from "./firebase.js";
 import { signInWithEmailAndPassword }
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 window.login = function () {
   const email = document.getElementById("email").value;
@@ -18,7 +18,7 @@ window.login = function () {
     .then(() => {
       window.location.href = "admin-dashboard.html";
     })
-    .catch(err => {
+    .catch((err) => {
       errorBox.innerText = err.message;
     });
 };
